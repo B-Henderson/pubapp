b570274691be7cb09a4e340583583daba9a176fe
b-henderson <benhenderson976@gmail.com>
2017-07-12 18:59:07 +0100

    setup translations and added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule, Http } from '@angular/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
+export function HttpLoaderFactory(http: Http) {
+    return new TranslateHttpLoader(http, 'assets/translations/', '-lang.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [Http]
+          }
+        })
+  ],
+  exports: [
+  	TranslateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
